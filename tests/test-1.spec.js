442c1878159c2d6fab2dce18f63004913a3fb47f
Hugo Batista <94hugobatista.com>
2026-01-20 17:52:34 -0300

    update

--- tests/test-1.spec.js
+++ tests/test-1.spec.js
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?zx=1768663139352&no_sw_cr=1');
-  await page.getByRole('img', { name: 'Google' }).click();
+// test('test', async ({ page }) => {
+//   await page.goto('https://www.google.com/?zx=1768663139352&no_sw_cr=1');
+//   await page.getByRole('img', { name: 'Google' }).click();
  
-});
+// });
